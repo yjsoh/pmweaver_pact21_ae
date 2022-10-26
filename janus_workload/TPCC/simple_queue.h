3ca0592d1a933b70b70b25356d4778a3bd7e9c32
Yun Joon Soh <yjsoh@eng.ucsd.edu>
2022-10-25 20:27:27 -0700

    Cleaned TPCC format

--- janus_workload/TPCC/simple_queue.h
+++ janus_workload/TPCC/simple_queue.h
@@ -1,72 +1,83 @@
 /*
   Author: Vaibhav Gogte <vgogte@umich.edu>
-          Aasheesh Kolli <akolli@umich.edu>
+		  Aasheesh Kolli <akolli@umich.edu>
 */
 
 //#include <iostream>
 #define QUEUE_SIZE 20
 
-class simple_queue {
-  private:
-    long entries[QUEUE_SIZE];
-    long head;
-    long tail;
-  public:
-    simple_queue() {
-      head = 0;
-      tail = 0;
-    }
+class simple_queue
+{
+private:
+	long entries[QUEUE_SIZE];
+	long head;
+	long tail;
 
-    ~simple_queue() {}
+public:
+	simple_queue()
+	{
+		head = 0;
+		tail = 0;
+	}
 
-    bool empty() {
-      return (head == tail);
-    }
+	~simple_queue() {}
 
-    bool full() {
-      if(tail == 0)
-        return (head == QUEUE_SIZE-1);
-      return (head == tail-1);
-    }
+	bool empty()
+	{
+		return (head == tail);
+	}
 
-    int size() {
-      if(head >= tail) {
-        return head - tail;
-      }
-      else {
-        return (QUEUE_SIZE - tail + head);
-      }
-    }
+	bool full()
+	{
+		if (tail == 0)
+			return (head == QUEUE_SIZE - 1);
+		return (head == tail - 1);
+	}
 
-    bool push(long entry) {
-      if(full())
-        return false;
-      entries[head] = entry;
-      if(head == QUEUE_SIZE-1)
-        head = 0;
-      else
-        head++;
-      return true;
-    }
+	int size()
+	{
+		if (head >= tail)
+		{
+			return head - tail;
+		}
+		else
+		{
+			return (QUEUE_SIZE - tail + head);
+		}
+	}
 
-    long front() {
-      return entries[tail];
-    }
+	bool push(long entry)
+	{
+		if (full())
+			return false;
+		entries[head] = entry;
+		if (head == QUEUE_SIZE - 1)
+			head = 0;
+		else
+			head++;
+		return true;
+	}
 
-    bool pop() {
-      if(empty())
-        return false;
-      if(tail == QUEUE_SIZE-1)
-        tail = 0;
-      else
-        tail++;
-      return true;
-    }
+	long front()
+	{
+		return entries[tail];
+	}
 
-    //void printQueue() {
-    //  std::cout<<"head tail "<<head<<" "<<tail<<std::endl;
-    //  for(int i=0; i<QUEUE_SIZE; i++) {
-    //    std::cout<<i<<" "<<entries[i]<<std::endl;
-    //  }
-    //}
+	bool pop()
+	{
+		if (empty())
+			return false;
+		if (tail == QUEUE_SIZE - 1)
+			tail = 0;
+		else
+			tail++;
+		return true;
+	}
+
+	// void printQueue() {
+	//   std::cout<<"head tail "<<head<<" "<<tail<<std::endl;
+	//   for(int i=0; i<QUEUE_SIZE; i++) {
+	//     std::cout<<i<<" "<<entries[i]<<std::endl;
+	//   }
+	// }
 };
