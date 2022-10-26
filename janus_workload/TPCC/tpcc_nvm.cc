61c9f39e053ad0519d6d366d0835c51a8fdfa8b8
Yun Joon Soh <yjsoh@eng.ucsd.edu>
2022-10-25 20:52:08 -0700

    TPCC: Copied the barrier header inside tpcc_nvm.cc

--- janus_workload/TPCC/tpcc_nvm.cc
+++ janus_workload/TPCC/tpcc_nvm.cc
@@ -17,6 +17,73 @@ This file models the TPCC benchmark.
 #include "common.h"
 #include "tpcc_db.h"
 
+/*
+ *   File: barrier.h
+ *   Author: Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>,
+ *  	     Tudor David <tudor.david@epfl.ch>
+ *   Description:
+ *   barrier.h is part of ASCYLIB
+ *
+ * Copyright (c) 2014 Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>,
+ * 	     	      Tudor David <tudor.david@epfl.ch>
+ *	      	      Distributed Programming Lab (LPD), EPFL
+ *
+ * ASCYLIB is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef BARRIER_H
+#define BARRIER_H
+
+/* ################################################################### *
+ * BARRIER
+ * ################################################################### */
+
+typedef struct barrier
+{
+	pthread_cond_t complete;
+	pthread_mutex_t mutex;
+	int count;
+	int crossing;
+} barrier_t;
+
+static inline void barrier_init(barrier_t *b, int n)
+{
+	pthread_cond_init(&b->complete, NULL);
+	pthread_mutex_init(&b->mutex, NULL);
+	b->count = n;
+	b->crossing = 0;
+}
+
+static inline void barrier_cross(barrier_t *b)
+{
+	pthread_mutex_lock(&b->mutex);
+	/* One more thread through */
+	b->crossing++;
+	/* If not all here, wait */
+	if (b->crossing < b->count)
+	{
+		pthread_cond_wait(&b->complete, &b->mutex);
+	}
+	else
+	{
+		pthread_cond_broadcast(&b->complete);
+		/* Reset for next time */
+		b->crossing = 0;
+	}
+	pthread_mutex_unlock(&b->mutex);
+}
+
+#endif /* BARRIER_H */
+
 #define NUM_ORDERS 100 // 10000000
 #define NUM_THREADS 1
 
@@ -28,7 +95,7 @@ TPCC_DB **tpcc_db; // array of TPCC_DB, array length == nthreads
 void init_db(uint64_t nthreads, uint64_t nwarehouse)
 {
 	*tpcc_db = (TPCC_DB *)aligned_malloc(64, nthread * sizeof(TPCC_DB));
-	for(uint64_t tid = 0; tid < nthreads; tid++)
+	for (uint64_t tid = 0; tid < nthreads; tid++)
 	{
 		tpcc_db[tid] = new TPCC_DB();
 		tpcc_db[tid]->initialize(nthreads, nwarehouse);
@@ -37,7 +104,7 @@ void init_db(uint64_t nthreads, uint64_t nwarehouse)
 
 void deinit_db()
 {
-	for(uint64_t tid = 0; tid < nthreads; tid++)
+	for (uint64_t tid = 0; tid < nthreads; tid++)
 	{
 		tpcc_db[tid]->deinitialize();
 		delete tpcc_db[tid];
