8775e781ff1627dfe1b52e4d241a61e8b9397666
Yun Joon Soh <yjsoh@eng.ucsd.edu>
2022-11-09 11:32:45 -0800

    TATP: Added pmdk results

--- janus_workload/TATP/tatp.sh
+++ janus_workload/TATP/tatp.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 domain=(adr eadr)
-binary=(clobber undo pmdk)
+binary=(pmdk)
 repeats=10
-
+args="1000000 0 1 10"
 # mkdir -p agr_tatp
 # (cd agr_tatp && ../../../../script/compile_and_execute.sh TATP 39_fence)
 
 for i in $(seq $repeats); do
 	for d in "${domain[@]}"; do
 		for b in "${binary[@]}"; do
+			sudo rm -f /mnt/ramdisk/*
 			# NSUBSCRIBER NOPS NTHREADS DURATION
 			if [[ $d == "eadr" ]]; then
-				sudo PMEM_NO_FLUSH=1 ./tatp.$d.$b 1000000 0 1 10
+				sudo PMEM_NO_FLUSH=1 ./tatp.$d.$b $args
 			elif [[ $d == "adr" ]]; then
-				sudo PMEM_NO_FLUSH=0 ./tatp.$d.$b 1000000 0 1 10
+				sudo PMEM_NO_FLUSH=0 ./tatp.$d.$b $args
 			fi
 		done
 	done
 
-	# sudo ./agr_tatp/TATP_39_fence 1000000 0 1 10
-	yj-noti "$i/$repeats"
+	yj-noti "[$0] $i/$repeats"
 done
